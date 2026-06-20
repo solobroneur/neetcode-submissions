66961bda377ce99e0b8bf3769c3aa19fce13909a
Solobroneur <development@solobroneur.com>
2026-06-20 17:50:22 -0400

    Add: sql-insert-constraints - submission-0

---
+++ SQL/sql-insert-constraints/submission-0.sql
@@ -0,0 +1,34 @@
+CREATE TYPE status AS ENUM ('active', 'inactive', 'pending');
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    age INTEGER CHECK (age >= 18),
+    status status
+);
+-- Do not modify above this line --
+
+
+INSERT INTO 
+    users (id, age, status, name) 
+VALUES
+  (1, 20, 'active', 'John Doe');
+
+INSERT INTO 
+    users (id, name, age, status) 
+VALUES
+  (2, 'Jane Doe', 27, 'pending');
+
+INSERT INTO 
+    users (id, name, age, status)
+VALUES
+  (3, 'John Smith', 28, 'active');
+
+INSERT INTO 
+    users (id, name, age, status) 
+VALUES
+  (4, 'Jane Smith', 30, 'inactive');
+
+
+-- Do not modify below this line --
+SELECT * FROM users;
